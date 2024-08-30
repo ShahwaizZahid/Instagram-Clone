b32b705b0c54a5b5053fd90ee97a65f5aff8d5eb
ShahwaizZahid <shahwaizmughal940@gmail.com>
2024-08-30 17:22:24 +0500

    refactor profile data show posted posts and follower

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,18 +1,220 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:isntragram_clone/data/model/usermodel.dart';
+import '../data/firebase_services/firestore.dart';
+import '../util/image_cached.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
-
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('profile'),),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey.shade100,
+        body: SafeArea(
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                child: FutureBuilder(
+                  future: Firebase_Firestor().getUser(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    return Head(snapshot.data!);
+                  },
+                ),
+              ),
+              StreamBuilder(
+                stream: _firebaseFirestore
+                    .collection('posts')
+                    .where('uid', isEqualTo: _auth.currentUser?.uid)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const SliverToBoxAdapter(
+                        child: Center(child: CircularProgressIndicator()));
+                  }
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  if (snapshot.data!.docs.isEmpty) {
+                    return const Center(child: Text('No posts available.'));
+                  }
+                  var snaplength = snapshot.data!.docs.length;
+                  return SliverGrid(
+                    delegate: SliverChildBuilderDelegate((context, index) {
+                      var snap = snapshot.data!.docs[index];
+                      return Container(
+                        child: CachedImage(snap['postImage']),
+                      );
+                    }, childCount: snaplength),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            crossAxisSpacing: 4,
+                            mainAxisSpacing: 4),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget Head(Usermodel user) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 5.h),
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 13.w, vertical: 10.h),
+                child: ClipOval(
+                  child: SizedBox(
+                    height: 80.h,
+                    width: 80.w,
+                    child: Container(child: CachedImage(user.profile)),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 17.w,
+                      ),
+                      Text(
+                        '0',
+                        style: TextStyle(
+                            fontSize: 16.sp, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        width: 53.w,
+                      ),
+                      Text(
+                        user.followers.length.toString(),
+                        style: TextStyle(
+                            fontSize: 16.sp, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        width: 53.w,
+                      ),
+                      Text(
+                        user.following.length.toString(),
+                        style: TextStyle(
+                            fontSize: 16.sp, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 30.w,
+                      ),
+                      Text(
+                        'Posts',
+                        style: TextStyle(
+                            fontSize: 13.sp, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        width: 30.w,
+                      ),
+                      Text(
+                        'Follower',
+                        style: TextStyle(
+                            fontSize: 13.sp, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        width: 30.w,
+                      ),
+                      Text(
+                        'Following',
+                        style: TextStyle(
+                            fontSize: 13.sp, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  user.username,
+                  style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 12.sp),
+                ),
+                SizedBox(
+                  height: 5.h,
+                ),
+                Text(
+                  user.bio,
+                  style:
+                      TextStyle(fontWeight: FontWeight.w500, fontSize: 12.sp),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.w),
+            child: Container(
+              alignment: Alignment.center,
+              height: 20.h,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5.r),
+                  border: Border.all(color: Colors.grey.shade400)),
+              child: const Text("Edit Your Profile"),
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 35.h,
+            child: const TabBar(
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey,
+              indicatorColor:
+                  Colors.black, // Color of the underline for the selected tab
+              indicatorWeight: 2.5, // Thickness of the underline
+              indicatorSize: TabBarIndicatorSize.tab,
+              labelPadding: EdgeInsets.symmetric(vertical: 8.0),
+              tabs: [
+                Icon(Icons.grid_on),
+                Icon(Icons.video_collection),
+                Icon(Icons.person)
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10.h,
+          )
+        ],
+      ),
     );
   }
 }
