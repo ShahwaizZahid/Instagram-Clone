baf4c3c4872f211230bd4989147902e71b08c7f2
ShahwaizZahid <shahwaizmughal940@gmail.com>
2024-09-01 19:12:02 +0500

    refactor: on reel show user details

--- lib/widgets/reels_item.dart
+++ lib/widgets/reels_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
+import 'package:isntragram_clone/util/image_cached.dart';
 import 'package:video_player/video_player.dart';
 
 class ReelsItem extends StatefulWidget {
@@ -31,11 +32,9 @@ class _ReelsItemState extends State<ReelsItem> {
       widget.snapshot['reelsvideo'],
       autoPlay: true,
       options: VlcPlayerOptions(
-        extras: [
-        ],
+        extras: [],
       ),
     );
-
   }
 
   @override
@@ -58,6 +57,7 @@ class _ReelsItemState extends State<ReelsItem> {
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.bottomRight,
       children: [
         InkWell(
           onTap: _togglePlayPause,
@@ -85,10 +85,97 @@ class _ReelsItemState extends State<ReelsItem> {
               ),
             ),
           ),
-        Column(
-          children: [
-            
-          ],
+        Positioned(
+          top: 430.h,
+          right: 15.w,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(
+                Icons.favorite_border,
+                color: Colors.white,
+                size: 28.w,
+              ),
+              SizedBox(height: 3.h),
+              Text(
+                '0',
+                style: TextStyle(fontSize: 12.sp, color: Colors.white),
+              ),
+              SizedBox(height: 15.h),
+              Icon(
+                Icons.comment,
+                color: Colors.white,
+                size: 28.w,
+              ),
+              SizedBox(height: 3.h),
+              Text(
+                '0',
+                style: TextStyle(fontSize: 12.sp, color: Colors.white),
+              ),
+              SizedBox(height: 15.h),
+              Icon(
+                Icons.send,
+                color: Colors.white,
+                size: 28.w,
+              ),
+              SizedBox(height: 3.h),
+              Text(
+                '0',
+                style: TextStyle(fontSize: 12.sp, color: Colors.white),
+              )
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 40.h,
+          left: 10.w,
+          right: 0,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  ClipOval(
+                    child: SizedBox(
+                      height: 35.h,
+                      width: 35.w,
+                      child: CachedImage(widget.snapshot['profileImage']),
+                    ),
+                  ),
+                  SizedBox(width: 10.w),
+                  Text(widget.snapshot['username'],
+                      style: TextStyle(
+                          fontSize: 13.sp,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(width: 10.w),
+                  Container(
+                    alignment: Alignment.center,
+                    width: 60.w,
+                    height: 25.h,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(5.r),
+                    ),
+                    child: Text(
+                      'Follow',
+                      style: TextStyle(
+                        fontSize: 13.sp,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 8.h),
+              Text(
+                widget.snapshot['caption'],
+                style: TextStyle(
+                  fontSize: 13.sp,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         )
       ],
     );
