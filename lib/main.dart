441cf81e5d1336407918c96174d91c411050c599
ShahwaizZahid <shahwaizmughal940@gmail.com>
2025-08-26 13:05:10 +0500

    chore: logo of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -17,11 +18,17 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ScreenUtilInit(
-        designSize: Size(375, 812),
-        child: MainPage(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AnimatedSplashScreen(
+          duration: 3000, // 3 seconds
+          splash: Image.asset("assets/icon/app.png"),
+          nextScreen: const MainPage(),
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor: Colors.white,
+        ),
       ),
     );
   }
